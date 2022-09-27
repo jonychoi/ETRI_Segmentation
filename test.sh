95f86c910b402aefa9934d784f2deecbb493f37e
SunwooKim <38250239+sunwoo76@users.noreply.github.com>
2022-09-28 02:05:27 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 CUDA_VISIBLE_DEVICES=0 python ./test.py \
---save_path "./checkpoints"\
---weights "./checkpoint/top6/source/Epoch::43:: | Model: etri_merge_top6  MIoU: 0.724 | MPA: 0.835083.pth" \
+--save_path "./checkpoints/"\
+--weights "./checkpoints/Epoch43_last_chekcpoint.pth" \
 --model_name "Segmento-ADE20k" \
 --top_k 6 \
 --batch_size 1 \
